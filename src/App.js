d9b6cd5c1fd5bd2f4a39214eb6c9616d5726a148
Mehedi Rabby <mehedi15-2995@diu.edu.bd>
2022-10-03 02:36:45 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1 className='text-4xl font-bold'>this is a big big header</h1>
+      <p>this is a paragraph</p>
+    </div>
+  );
+}
+
+export default App;
